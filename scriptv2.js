3a9f0fa34b2bbcdca5052d17ffd1d7fb56773ed0
ajfilho <achraffilho@gmail.com>
2026-02-18 16:44:29 -0300

    Update scriptv2.js

--- scriptv2.js
+++ scriptv2.js
@@ -1,14 +1,13 @@
-console.log("Script versão 2.0 carregado!");
+WA.onInit().then(() => {
+  console.log("Script ok / WA pronto");
 
-// Quando o avatar PISAR no quadrado invisível
-WA.room.onEnterZone('zona_reuniao', () => {
-    console.log("PISOU NO TAPETE! Apagando as luzes...");
-    // Adicionamos a pasta 'Sala 01' antes do nome da camada
-    WA.room.showLayer('Sala 01/dark_overlay');
-});
+  WA.room.area.onEnter("zona_reuniao").subscribe(() => {
+    console.log("ENTROU na area");
+    WA.room.showLayer("salas/dark_overlay");
+  });
 
-// Quando o avatar SAIR do quadrado invisível
-WA.room.onLeaveZone('zona_reuniao', () => {
-    console.log("SAIU DO TAPETE! Acendendo as luzes...");
-    WA.room.hideLayer('Sala 01/dark_overlay');
+  WA.room.area.onLeave("zona_reuniao").subscribe(() => {
+    console.log("SAIU da area");
+    WA.room.hideLayer("salas/dark_overlay");
+  });
 });
